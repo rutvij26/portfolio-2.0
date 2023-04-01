d45332d63fc1c97fdb4a7c0fc259d8840ea16963
Rutvij Sathe <rut26sathe@gmail.com>
2023-04-01 15:50:53 -0400

    Bug Fix in Hero Component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ function Hero({ }: Props) {
                 <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Full Stack Engineer</h2>
                 <h1 className="text-5xl lg:text-6xl font-semibold px-10">
                     <span className='mr-3'>{text}</span>
-                index.ts    <Cursor cursorColor='#F7AB0A' />
+                <Cursor cursorColor='#F7AB0A' />
                 </h1>
                 <div className="pt-5">
                     <Link href="#about">
